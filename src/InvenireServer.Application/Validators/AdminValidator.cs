6630224f776c4a295585d50f05b991ec621ff37b
realtobi999 <tobiasfilgas@gmail.com>
2025-06-08 23:11:55 +0200

    [built] admin validator, tested

---
+++ src/InvenireServer.Application/Validators/AdminValidator.cs
@@ -0,0 +1,35 @@
+using InvenireServer.Application.Interfaces.Common;
+using InvenireServer.Application.Interfaces.Managers;
+using InvenireServer.Domain.Entities;
+using InvenireServer.Domain.Exceptions.Http;
+
+namespace InvenireServer.Application.Validators;
+
+public class AdminValidator : IValidator<Admin>
+{
+    private readonly IRepositoryManager _repositories;
+
+    public AdminValidator(IRepositoryManager repositories)
+    {
+        _repositories = repositories;
+    }
+
+    public async Task<(bool isValid, Exception? exception)> ValidateAsync(Admin admin)
+    {
+        // Email address must be unique.
+        if (await _repositories.Admins.GetAsync(e => e.EmailAddress == admin.EmailAddress && e.Id != admin.Id) is not null) return (false, new BadRequest400Exception($"{nameof(Employee.EmailAddress)} must be unique among all employees."));
+
+        // Last login must be later than creation time, if set.
+        if (admin.LastLoginAt is not null && admin.CreatedAt >= admin.LastLoginAt) return (false, new BadRequest400Exception($"{nameof(Employee.LastLoginAt)} must be later than CreatedAt."));
+
+        // Last update must be later than creation time, if set.
+        if (admin.LastUpdatedAt is not null && admin.CreatedAt >= admin.LastUpdatedAt) return (false, new BadRequest400Exception($"{nameof(Employee.LastUpdatedAt)} must be later than CreatedAt."));
+
+        // Creation time cannot be set in the future.
+        if (admin.CreatedAt > DateTimeOffset.UtcNow) return (false, new BadRequest400Exception($"{nameof(Employee.CreatedAt)} cannot be set in the future."));
+
+        // TODO: Validate that the referenced organization exists.
+
+        return (true, null);
+    }
+}
---
+++ tests/InvenireServer.Tests/Unit/Validators/AdminValidatorTests.cs
@@ -0,0 +1,95 @@
+using InvenireServer.Application.Factories.Admins;
+using InvenireServer.Application.Interfaces.Managers;
+using InvenireServer.Application.Validators;
+using InvenireServer.Domain.Entities;
+using InvenireServer.Domain.Exceptions.Http;
+using InvenireServer.Tests.Integration.Fakers;
+
+namespace InvenireServer.Tests.Unit.Validators;
+
+public class AdminValidatorTests
+{
+    private readonly Mock<IRepositoryManager> _repository;
+    private readonly AdminValidator _validator;
+
+    public AdminValidatorTests()
+    {
+        _repository = new Mock<IRepositoryManager>();
+        _validator = new AdminValidator(_repository.Object);
+    }
+
+    [Fact]
+    public async Task ValidateAsync_ReturnsFalseWhenEmailIsNotUnique()
+    {
+        // Prepare.
+        var admin = new AdminFaker().Generate();
+
+        // Mock the repository to return a employee that has seemingly the same email address.
+        _repository.Setup(r => r.Admins.GetAsync(e => e.EmailAddress == admin.EmailAddress && e.Id != admin.Id)).ReturnsAsync(new AdminFaker().Generate());
+
+        // Act & Assert.
+        var (valid, exception) = await _validator.ValidateAsync(admin);
+
+        valid.Should().BeFalse();
+        exception.Should().BeOfType<BadRequest400Exception>();
+        exception!.Message.Should().Contain(nameof(Employee.EmailAddress));
+    }
+
+    [Fact]
+    public async Task ValidateAsync_ReturnsFalseWhenUpdatedAtIsSetBeforeCreatedAt()
+    {
+        // Prepare.
+        var admin = new AdminFaker().Generate();
+
+        // Set the updated at time to be before the created at time.
+        admin.LastUpdatedAt = admin.CreatedAt.AddMonths(-1);
+
+        _repository.Setup(r => r.Admins.GetAsync(e => e.EmailAddress == admin.EmailAddress && e.Id != admin.Id)).ReturnsAsync((Admin?)null);
+
+        // Act & Assert.
+        var (valid, exception) = await _validator.ValidateAsync(admin);
+
+        valid.Should().BeFalse();
+        exception.Should().BeOfType<BadRequest400Exception>();
+        exception!.Message.Should().Contain(nameof(Employee.LastUpdatedAt));
+    }
+
+    [Fact]
+    public async Task ValidateAsync_ReturnsFalseWhenLastLoginAtIsSetBeforeCreatedAt()
+    {
+        // Prepare.
+        var admin = new AdminFaker().Generate();
+
+        // Set the last login time to be before the created at time.
+        admin.LastLoginAt = admin.CreatedAt.AddMonths(-1);
+
+        _repository.Setup(r => r.Admins.GetAsync(e => e.EmailAddress == admin.EmailAddress && e.Id != admin.Id)).ReturnsAsync((Admin?)null);
+
+        // Act & Assert.
+        var (valid, exception) = await _validator.ValidateAsync(admin);
+
+        valid.Should().BeFalse();
+        exception.Should().BeOfType<BadRequest400Exception>();
+        exception!.Message.Should().Contain(nameof(Employee.LastLoginAt));
+    }
+
+    [Fact]
+    public async Task ValidateAsync_ReturnsFalseWhenCreatedAtIsSetInTheFuture()
+    {
+        // Prepare.
+        var admin = new AdminFaker().Generate();
+
+        // Set the created at time to be in the future.
+        admin.CreatedAt = DateTimeOffset.UtcNow.AddMonths(1);
+        admin.LastUpdatedAt = null;
+
+        _repository.Setup(r => r.Admins.GetAsync(e => e.EmailAddress == admin.EmailAddress && e.Id != admin.Id)).ReturnsAsync((Admin?)null);
+
+        // Act & Assert.
+        var (valid, exception) = await _validator.ValidateAsync(admin);
+
+        valid.Should().BeFalse();
+        exception.Should().BeOfType<BadRequest400Exception>();
+        exception!.Message.Should().Contain(nameof(Employee.CreatedAt));
+    }
+}
